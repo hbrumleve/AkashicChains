8723bf1e16fa50e08a7d481df7fbe09f15f9fe6b
Harry Brumleve <hbrumleve@gmail.com>
2018-01-15 18:49:32 -0700

    added longitudinal aspects

---
+++ AkashicChains/AkashicChains.Core/LongitudinalEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AkashicChains.Core
+{
+    public class LongitudinalEvaluator
+    {
+        public string Name { get; private set; }
+
+        public string Evaluator { get; private set; }
+
+        private LongitudinalEvaluator(string name, string evaluator)
+        {
+            Name = name;
+            Evaluator = evaluator;
+        }
+
+        public static LongitudinalEvaluator Build(string name, string evaluator)
+        {
+            // parse the evaluator with JINT
+            return new LongitudinalEvaluator(name, evaluator);
+        }
+    }
+}
